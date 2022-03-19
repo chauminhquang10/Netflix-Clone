2cc533cbfd4f7d4a1f6b5f4b375ec213c4fad059
RiverO2442 <66553338+RiverO2442@users.noreply.github.com>
2022-03-19 16:58:04 +0700

    notify update, some change

--- client_official/src/components/main_pages/utils/movie_news/Movie_News_Item.js
+++ client_official/src/components/main_pages/utils/movie_news/Movie_News_Item.js
@@ -3,43 +3,69 @@ import "./Movie_News_Item.scss";
 import moment from "moment";
 import { Link } from "react-router-dom";
 import CancelIcon from "@mui/icons-material/Cancel";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 const Movie_News_Item = ({
   notify,
   handleIsRead,
   handleDeleteSingleNotify,
+  isRead,
 }) => {
   return (
     <div className="Movie_News_Item_container">
-      <Link
-        className="Movie_News_Item_Link"
-        to={`${notify.url}`}
-        onClick={() => handleIsRead(notify, notify.seenUsers)}
-        style={{ textDecoration: "none" }}
+      <div
+        className={
+          isRead
+            ? "Movie_News_Item_check_icon"
+            : "Movie_News_Item_check_icon active"
+        }
       >
-        <img className="Movie_News_Item_img" src={notify.image} />
-      </Link>
-      <Link
-        className="Movie_News_Item_Link"
-        to={`${notify.url}`}
-        onClick={() => handleIsRead(notify, notify.seenUsers)}
-        style={{ textDecoration: "none" }}
-      >
-        <div className="Movie_News_Item_info">
-          <div className="Movie_News_Item_info_title">
-            New Content !!!
-            <br></br>
-            {notify.content.slice(0, 20)}
+        <FiberManualRecordIcon />
+      </div>
+      <div>
+        <Link
+          className="Movie_News_Item_Link"
+          to={`${notify.url}`}
+          onClick={() => handleIsRead(notify, notify.seenUsers)}
+          style={{ textDecoration: "none" }}
+        >
+          <img className="Movie_News_Item_img" src={notify.image} />
+        </Link>
+      </div>
+      <div>
+        <Link
+          className="Movie_News_Item_Link"
+          to={`${notify.url}`}
+          onClick={() => handleIsRead(notify, notify.seenUsers)}
+          style={{ textDecoration: "none" }}
+        >
+          <div className="Movie_News_Item_info">
+            <div className="Movie_News_Item_info_title">{notify.content}</div>
+            <div className="Movie_News_Item_info_desc_container">
+              <div className="Movie_News_Item_info_desc">{notify.text}</div>
+            </div>
+            <div className="Movie_News_Item_info_time">
+              {moment(notify.createdAt).fromNow()}
+            </div>
           </div>
-          <div className="Movie_News_Item_info_time">
-            {moment(notify.createdAt).fromNow()}
+        </Link>
+      </div>
+      <div className="Movie_News_Item_info_icon">
+        <MoreVertIcon fontSize="large" />
+        <div className="Movie_News_Item_info_icon_drop_list">
+          <div
+            className="Movie_News_Item_info_icon_drop_list_option"
+            onClick={() => handleDeleteSingleNotify(notify)}
+          >
+            Delete
+          </div>
+          <div
+            className="Movie_News_Item_info_icon_drop_list_option"
+            onClick={() => handleIsRead(notify, notify.seenUsers)}
+          >
+            Mark As Read
           </div>
         </div>
-      </Link>
-      <div className="Movie_News_Item_info_icon">
-        <CancelIcon
-          fontSize="large"
-          onClick={handleDeleteSingleNotify(notify)}
-        />
       </div>
     </div>
   );
