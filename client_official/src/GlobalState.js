a581237d2830de112bcef5854804164f4e919f0d
RiverO2442 <66553338+RiverO2442@users.noreply.github.com>
2022-07-16 09:39:21 +0700

    delete unused package

--- client_official/src/GlobalState.js
+++ client_official/src/GlobalState.js
@@ -34,7 +34,7 @@ export const DataProvider = ({ children }) => {
     // auto refresh ranking movies
     setTimeout(() => {
       refreshNewRankingMovies();
-    }, 1 * 60 * 1000);
+    }, 10 * 60 * 1000);
   };
 
   useEffect(() => {
